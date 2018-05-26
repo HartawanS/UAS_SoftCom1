3b482eb7e4c228101e65e40c0b0377c6bc135415
Hartawan\wawan <hartawan_sugihono@yahoo.com>
2018-05-26 14:13:14 +0700

    file string []

--- src/uas_softcom1/FrameHome.java
+++ src/uas_softcom1/FrameHome.java
@@ -50,16 +50,8 @@ public class FrameHome extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jSpinnerInputNeuron = new javax.swing.JSpinner();
-        jSpinnerHiddenNeuron = new javax.swing.JSpinner();
-        jSpinnerOutputNeuron = new javax.swing.JSpinner();
         jLabel4 = new javax.swing.JLabel();
         jSpinnerLearningRate = new javax.swing.JSpinner();
-        jLabel5 = new javax.swing.JLabel();
-        jSpinnerIteration = new javax.swing.JSpinner();
         jTextFieldTrainInput = new javax.swing.JTextField();
         jTextFieldTrainOutput = new javax.swing.JTextField();
         jButtonTrain = new javax.swing.JButton();
@@ -70,25 +62,9 @@ public class FrameHome extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(153, 255, 255));
 
-        jLabel1.setText("Input Neuron: ");
-
-        jLabel2.setText("Hidden Neuron :");
-
-        jLabel3.setText("Output Neuron :");
-
-        jSpinnerInputNeuron.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
-
-        jSpinnerHiddenNeuron.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
-
-        jSpinnerOutputNeuron.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
-
         jLabel4.setText("Learning Rate :");
 
         jSpinnerLearningRate.setModel(new javax.swing.SpinnerNumberModel(0, 0, null, 1));
-
-        jLabel5.setText("Iteration");
-
-        jSpinnerIteration.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
 
         jTextFieldTrainInput.setEditable(false);
 
@@ -121,21 +97,9 @@ public class FrameHome extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jTextFieldTrainInput)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel3)
-                                    .addComponent(jLabel4)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jLabel1))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jSpinnerInputNeuron)
-                                    .addComponent(jSpinnerHiddenNeuron)
-                                    .addComponent(jSpinnerLearningRate, javax.swing.GroupLayout.DEFAULT_SIZE, 58, Short.MAX_VALUE)
-                                    .addComponent(jSpinnerOutputNeuron)))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel5)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jSpinnerIteration, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 134, Short.MAX_VALUE)
+                                .addComponent(jSpinnerLearningRate, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addComponent(jTextFieldTrainOutput)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -155,27 +119,11 @@ public class FrameHome extends javax.swing.JFrame {
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(42, 42, 42)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jSpinnerInputNeuron, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jSpinnerHiddenNeuron, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(jSpinnerOutputNeuron, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(135, 135, 135)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel4)
                     .addComponent(jSpinnerLearningRate, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
-                    .addComponent(jSpinnerIteration, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(27, 27, 27)
+                .addGap(58, 58, 58)
                 .addComponent(jButtonTrainInput)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jTextFieldTrainInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -194,7 +142,7 @@ public class FrameHome extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 263, Short.MAX_VALUE))
+                .addGap(0, 76, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -220,14 +168,14 @@ public class FrameHome extends javax.swing.JFrame {
              while(inputStream.hasNext()){
              String data= inputStream.next();
              String[] values = data.split(",");
-             System.out.println(values[1]);
+             System.out.println(values[0]+"\t"+values[1]+"\t"+values[2]);
              }
              inputStream.close();
           }catch (FileNotFoundException e) {
              e.printStackTrace();
      }
      //This prints out the working directory
-     System.out.println("Present Project Directory : "+ System.getProperty("user.dir"));
+     System.out.println("\n\n Present Project Directory : "+ System.getProperty("user.dir"));
 
           
     }//GEN-LAST:event_jButtonTrainInputActionPerformed
@@ -271,17 +219,9 @@ public class FrameHome extends javax.swing.JFrame {
     private javax.swing.JButton jButtonTrain;
     private javax.swing.JButton jButtonTrainInput;
     private javax.swing.JButton jButtonTrainOutput;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JSpinner jSpinnerHiddenNeuron;
-    private javax.swing.JSpinner jSpinnerInputNeuron;
-    private javax.swing.JSpinner jSpinnerIteration;
     private javax.swing.JSpinner jSpinnerLearningRate;
-    private javax.swing.JSpinner jSpinnerOutputNeuron;
     private javax.swing.JTextField jTextFieldTrainInput;
     private javax.swing.JTextField jTextFieldTrainOutput;
     // End of variables declaration//GEN-END:variables
